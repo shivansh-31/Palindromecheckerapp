d0b806de0f294d04277b150566d3a03fb03b9102
shivansh-31 <shivansh.31m@gmail.com>
2026-02-27 09:32:21 +0530

    UC1: Add welcome message module

--- PalindromecheckerApp/src/PalindromeCheckerApp.java
+++ PalindromecheckerApp/src/PalindromeCheckerApp.java
@@ -1,2 +1,56 @@
+/**
+ * MAIN CLASS: UseCase1; PalindromeCheckerApp
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * Description: This class shows the entry point of the PCMS (Palindrome Checker Management System)
+ * At this stage, the application consists of:
+ *  - It executes in the main() method
+ *  - Displays opening message of the app
+ *  - Shows the system version
+ *
+ *  No palindrome logic has been implemented yet
+ *  Our Goal is to establish a opening message or clear startup flow
+ *
+ * @author
+ * @version 1.0
+ *
+ * */
+
 public class PalindromeCheckerApp {
+    /**
+     *Palindrome
+     * */
+    public static void main(String[] args){
+        System.out.println("\nWelcome to the Palindrome Checker Management System");
+        System.out.println("Version: 1.0");
+        System.out.println("System initialized Successfully");
+    }
+}
+/**
+ * MAIN CLASS: UseCase1; PalindromeCheckerApp
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * Description: This class shows the entry point of the PCMS (Palindrome Checker Management System)
+ * At this stage, the application consists of:
+ *  - It executes in the main() method
+ *  - Displays opening message of the app
+ *  - Shows the system version
+ *
+ *  No palindrome logic has been implemented yet
+ *  Our Goal is to establish a opening message or clear startup flow
+ *
+ * @author
+ * @version 1.0
+ *
+ * */
+
+public class PalindromeCheckerApp {
+    /**
+     *Palindrome
+     * */
+    public static void main(String[] args){
+        System.out.println("\nWelcome to the Palindrome Checker Management System");
+        System.out.println("Version: 1.0");
+        System.out.println("System initialized Successfully");
+    }
 }
